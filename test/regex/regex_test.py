2441ce46bf40557dbbc7e47652c76dc607ee10bc
MOoop <MOoopjjang@gmail.com>
2018-09-10 19:48:20 +0900

    no message

--- test/regex/regex_test.py
+++ test/regex/regex_test.py
@@ -43,8 +43,10 @@ def test2():
 	sales1.xls
 	order3.xls
 	sales2.xls
+	ca7.xls
 	sales3.xls
 	apac1.xls
+	ca10.xls
 	europe2.xls
 	sam.xls
 	ca1.xls
@@ -73,13 +75,72 @@ def test2():
 	}
 	"""
 
-	print('{}'.format(text5))
+
+	text6 = """
+	'101','Ben','Forta'
+	'102','Jim','James'
+
+	'103','Roberta','Robertson'
+	'104','Bob','Bobson'
+	"""
+
+	text7 = """
+	11213
+	A1C2E3
+	48075
+	78237
+	M1B4F2
+	90046
+	H1H2H2
+	"""
 
 
-	rgx = re.compile('[ns]a[^0-9]\.xls')  # text1
+	text8 = """
+	Send personal email to ben@forta.com. For questions
+	about a book use support@forta.com. Feel free to send
+	unsolicited email to spam@forta.com (wouldn't it be
+	nice if it were that simple, huh?).
+	"""
+
+	text9 = """
+	Send personal  bbb.ccc.ccc@ttt.com email to ben@forta.com or
+	ben.forta@forta.com. For questions about a
+	book use support@forta.com. If your message
+	is urgent try ben@urgent.forta.com. Feel
+	free to send unsolicited email to
+	spam@forta.com (wouldn't it be nice if
+	it were that simple, huh?).
+	"""
+
+	text10 = """
+	Hello .ben@forta.com is my email address.
+	"""
+
+	text11= """
+	The URL is http://www.forta.com/ , to connect
+	securely use https://www.forta.com/ instead.
+	"""
+
+	text12 = """
+	4/8/03
+	10-6-2004
+	2/2/2
+	01-01-01
+	"""
+
+	print('{}'.format(text12))
+
+
+	# rgx = re.compile('[ns]a[^0-9]\.xls')  # text1
 	# rgx = re.compile('#[0-9A-Z]{6}') #text4
-	rgx = re.compile('array\[0\]')
-	mo = rgx.findall(text5)
+	# rgx = re.compile('[cn]a[\d]{1,}\.xls')
+	# rgx = re.compile('\r\n\r\n') #text6
+	# rgx = re.compile('\w\d\w\d\w\d') #text7
+	# rgx = re.compile('\w+[\w.]*@[\w\.]+\.\w+') #text8
+	# rgx = re.compile('https?://[\w.]+\.\w+/') #text11
+	# rgx = re.compile('#[\w\d]{6}') #text11
+	rgx = re.compile('\d{1,2}[-\/]\d{1,2}[-\/]\d{2,4}') #text12
+	mo = rgx.findall(text12)
 	if mo != None:print('{}'.format(mo))
 
 
