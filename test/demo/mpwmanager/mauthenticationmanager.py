2bf1ce36fc539f1e686e64267b7b93fcdacd71bf
[지나웍스] 김철우 <cwkim@zinnaworks.com>
2020-01-28 11:36:11 +0900

    ing

---
+++ test/demo/mpwmanager/mauthenticationmanager.py
@@ -0,0 +1,11 @@
+#!python3
+#-*- coding:utf-8 -*-
+
+'''
+ - user 등록 / 인증 기능제공
+ - 계정정보를 MDataAccessManager를 통해 저장/삭제/편집을 할수 있다.
+'''
+
+
+
+class MAuthenticationManager:pass
---
+++ test/demo/mpwmanager/mdataaccessmanager.py
@@ -0,0 +1,9 @@
+#!python3
+#-*- coding:utf-8 -*-
+
+
+'''
+Data를 db | file로 저장/수정/삭제/read하는 기능을 제공
+'''
+
+class MDataAccessManager:pass
