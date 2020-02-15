74e137199e335e1e4ce127e1f100350844c795f2
[지나웍스] 김철우 <cwkim@zinnaworks.com>
2020-02-16 01:00:44 +0900

    ing

---
+++ test/demo/mpwmanager/authentication.py
@@ -0,0 +1,41 @@
+#!python3
+#-*- coding:utf-8 -*-
+
+'''
+	인증 객체
+'''
+
+import bcrypt
+
+
+class Authentication:
+	def __init__( self , email = None , pwd = None):
+		self._email = email
+		self._salt = bcrypt.gensalt()
+		self._pwd = bcrypt.hashpw(pwd.encode() , self._salt )
+
+	def __repr__( self ):
+		return 'email : %s , pwd : %s'%(self._email , self._pwd)
+
+
+	def getEmail( self ):
+		return self._email
+
+	def getPwd( self ):
+		return self._pwd
+
+	def matched( self , rawPwd ):
+		return bcrypt.checkpw( rawPwd.encode() , self._pwd)
+
+
+
+if __name__ == '__main__':
+	auth = Authentication('xferlog@naver.com' , '1111')
+	print(auth)
+
+	inputpw = input('p : ')
+	if auth.matched(inputpw):
+		print('matched')
+	else:
+		print('not matched')
+
--- test/demo/mpwmanager/mauthenticationmanager.py
+++ test/demo/mpwmanager/mauthenticationmanager.py
@@ -7,5 +7,35 @@
 '''
 
 
+from authentication import Authentication
 
-class MAuthenticationManager:pass
+class MAuthenticationManager:
+	def __init__( self ):
+		self._members = []
+
+
+	def createMember( self , email = None , pwd = None):
+		'''
+		새로운 사용자를 등록한다.
+		'''
+		if email == None or pwd == None:
+			raise Exception('파라미터 오류입니다.')
+
+		if self._existMember_( email ):
+			raise Exception('{} 는 이미등록된 사용자입니다.'.format(email))
+
+		#등록
+		self._members.append(Authentication(email , pwd))
+
+
+
+
+	def _existMember_( self , email ):
+		if len(self._members) == 0:return True
+
+		for m in self._members:
+			if m.getEmail() == email:
+				return True
+		return False
+
+
