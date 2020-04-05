2b9c5e70f199cac21dff112b81f4294bbcc87646
[지나웍스] 김철우 <cwkim@zinnaworks.com>
2020-04-05 21:30:44 +0900

    ing

--- test/demo/mpwmanager/person_information.py
+++ test/demo/mpwmanager/person_information.py
@@ -11,3 +11,17 @@ class PersonInformation:
 	def __init__( self  , _id):
 		self._id = _id
 		self._infos = []
+
+
+	def __iter__( self ):
+		for info in self._infos:
+			yield info
+
+
+	def __getitem__( self  , x ):
+		return self._infos[x]
+
+
+
+	def add( self , _info ):
+		self._infos.append(_info)
