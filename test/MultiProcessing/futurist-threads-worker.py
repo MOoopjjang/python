32ee98df5c8bde7e4d4a79b3c67d004d695bc472
[지나웍스] 김철우 <cwkim@zinnaworks.com>
2020-04-08 16:56:49 +0900

    ing

--- test/MultiProcessing/futurist-threads-worker.py
+++ test/MultiProcessing/futurist-threads-worker.py
@@ -1,40 +1,25 @@
 #!python3
 #-*- coding:utf-8 -*-
 
-
+'''
+	- 통계정보제공 ( .statistics)
+'''
 
 import random
 import futurist
 from futurist import waiters
-# from concurrent.futures import ThreadPoolExecutor
-# from concurrent.futures import ProcessPoolExecutor
 
 
-
-def conpute():
-	return sum([random.randint(1,100) for _ in range(1000000)])
-
+def compute():
+	return sum( [random.randint(1,1000) for _ in range(100000)])
 
 
+with futurist.ThreadPoolExecutor(max_workers = 8 ) as worker:
+	futures = [ worker.submit(compute) for _ in range(8) ]
+	print(worker.statistics)
 
-def main():
+results = waiters.wait_for_all(futures)
+print(worker.statistics)
 
-	# ts = None
-	with futurist.ThreadPoolExecutor(max_workers = 8) as executors:
-		ts = [ executors.submit(conpute) for _ in range(8) ]
-		print(executors.statistics)
-
-
-	results = waiters.wait_for_all(ts)
-
-	# 사용중인 실행자에 관한 통계(statistics)를 제공
-	# 작업의 현재 상태를 추적하고 어떻게 코드가 실행되는지에 관한 정보를 얻을때 유용하다.
-	print(executors.statistics)
-
-	print('results : {}'.format([ r.result() for r in results.done]))
-
-
-
-if __name__ == '__main__':
-	main()
+print('result : {}'.format([r.result() for r in results.done]))
 
