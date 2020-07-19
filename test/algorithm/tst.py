6240b3333c713ebe7d35333f040456ae0c46017a
김철우 <>
2020-07-19 19:47:40 +0900

    ing

--- test/algorithm/tst.py
+++ test/algorithm/tst.py
@@ -95,24 +95,65 @@ def binary_search( org , n):
 	return searching( org , n )
 
 
+def sum_value_2():
+	'''
+	범위값의 합
+	'''
+	input_num = int(input('>>'))
+	v = input_num*(input_num+1)//2
+	print('v : {}'.format(v))
+
+def max_value():
+	import timeit
+	'''
+	최대값 반환
+	'''
+	def type1(_ar):
+		s = timeit.default_timer()
+
+		num = 0
+		for v in _ar:
+			if num < v:
+				num = v
+		print('duration : {}'.format(timeit.default_timer() - s))
+		return num
+
+	def type2(_ar):
+		def _max_value(_ar):
+			print('_ar : {}'.format(_ar))
+			n = 0
+			for v in _ar:
+				if n < v:n = v
+			return n
+
+		s = timeit.default_timer()
+		print('_ar:{}'.format(_ar))
+		dd = len(_ar)//2
+		n1 = _max_value(_ar[0:dd])
+		n2 = _max_value(_ar[dd:len(_ar)])
+		print('duration : {}'.format(timeit.default_timer() - s))
+		return n1 if n1>n2 else n2
+
+
+	#num = type1([10,3,2,99,12,13,55,21,9])
+	#num = type2([10,3,2,99,12,13,55,21,9])
+
+	num = type1([i for i in range(1000000)])
+#	num = type2([i for i in range(1000000)])
+	return num
 
 
 
 if __name__ == '__main__':
 	# result = fact(1 , 10 , 1)
 	# print('result : {}'.format(result))
-
-
 	# print('{}'.format(fact1(5)))
-
 
 	# gcd(22,11)
 
+	#result = binary_search([1,2,3,12,839,898,8,0,23,11,90] , 111)
+	#print('result : {}'.format(result))
 
-	result = binary_search([1,2,3,12,839,898,8,0,23,11,90] , 111)
-	print('result : {}'.format(result))
-
-
-
-
+#	sum_value_2()
+	print('max : {}'.format(max_value()))
 
