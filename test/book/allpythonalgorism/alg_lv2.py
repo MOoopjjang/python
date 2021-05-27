b6b90514cd3546bf2641c8f0b3bb60574adf592e
[지나웍스] 김철우 <cwkim@zinnaworks.com>
2021-05-27 20:31:43 +0900

    ing

--- test/book/allpythonalgorism/alg_lv2.py
+++ test/book/allpythonalgorism/alg_lv2.py
@@ -14,20 +14,20 @@ def group_anagrams():
     ]
     '''
 
-    def isContain(_output , _v):
+    def isContain(_output, _v):
         cIndex = -1
         if len(_output) == 0:
             return cIndex
 
         lv = list(_v)
         lv.sort()
-        for  index , ar in enumerate(_output):
+        for index, ar in enumerate(_output):
             text = ar[0]
-            if len(text) != len(_v):continue
+            if len(text) != len(_v): continue
 
             tl = list(text)
             tl.sort()
-            if tl != lv:continue
+            if tl != lv: continue
 
             cIndex = index
             break
@@ -36,8 +36,8 @@ def group_anagrams():
 
     def in_group_anagrams(_ar):
         output = []
-        for i , v in enumerate(_ar):
-            cIndex = isContain(output,v)
+        for i, v in enumerate(_ar):
+            cIndex = isContain(output, v)
             if cIndex == -1:
                 vv = []
                 vv.append(v)
@@ -47,13 +47,8 @@ def group_anagrams():
                 cAr.append(v)
         return output
 
-
-    result = in_group_anagrams(["eat" , "tea" , "tan" , "ate", "nat","bat"])
+    result = in_group_anagrams(["eat", "tea", "tan", "ate", "nat", "bat"])
     print(f'>>>>>>>>>>> {result} <<<<<<<<<<<<<')
-
-
-
-
 
 
 def longest_palindromic_substring():
@@ -61,9 +56,18 @@ def longest_palindromic_substring():
     가장 긴 팰린드롬 부분 문자열을 출력하라.
     입력: "babad"
     출력: "bab"
+
+    progress : ing
     '''
 
-
+    def isPalindrom(_ar, _s, _e):
+        print(f'_ar : {_ar} , _s : {_s} , _e : {_e}')
+        while _e > _s:
+            if _ar[_s] != _ar[_e]:
+                return False
+            _s += 1
+            _e -= 1
+        return True
 
 
 if __name__ == '__main__':
