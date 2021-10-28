a7dea7aff733582e80cfbf7ee7a1f8bd8c8d3d64
[지나웍스] 김철우 <cwkim@zinnaworks.com>
2021-10-28 18:42:52 +0900

    ing

---
+++ test/webserver/ch2/mysite/my_settings.py
@@ -0,0 +1,11 @@
+
+DATABASES = {
+    'default' : {
+        'ENGINE' : 'django.db.backends.mysql'
+        ,'NAME' : 'CWKIM_DB'
+        ,'USER' : 'cwkim'
+        ,'PASSWORD' : 'cwkim1234'
+        ,'HOST':'localhost'
+        ,'PORT':'3306'
+    }
+}
